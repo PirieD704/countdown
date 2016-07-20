58026b78e17f53f229e2c0eeeae84b6d5fdb9239
PirieD704 <dp75975@gmail.com>
2016-07-20 16:55:37 -0400

    some updates to features have not revamped theme in this one

--- readme.md
+++ readme.md
@@ -5,3 +5,5 @@ This is a basic time that uses setInterval to countdown from the present moment 
 Weeks, days, hours, minutes, and seconds are targeted individually to provide a coherent breakdown of the remaining time.
 
 bonus feature was a line up list that cycles a new artist each second and repeats once the list is complete
+
+now includes feature of user entering their own date thereby setting the counter for themselves.
--- scripts.js
+++ scripts.js
@@ -5,15 +5,32 @@ function update(){
 // Get the current time in millisecond since 1970 (UNIX epoch)
 var now = Date.now();
 // var rioStart = Date.parse('September 17, 2016 16:00');
-var rioStart = new Date('September 17, 2016 16:00');
-// Time in seconds between the current moment and the start of Rio 2016
-var getTimeTillRio = (rioStart - now) / 1000;
+// var rioStart = new Date('September 17, 2016 16:00');
 
+var userMonth = document.getElementById('month').value;
+var userDay = document.getElementById('day').value;
+var userYear = document.getElementById('year').value;
+var userHour = document.getElementById('hours').value;
+var userMinute = document.getElementById('minutes').value;
+
+var userStart = userMonth + " " + userDay + ", " + userYear + " " + userHour + ":" + userMinute;
+var userDate = new Date(userStart);
+
+console.log(userDate);
+console.log(userStart);
+
+// Time in seconds between the current moment and the start of Rio 2016
+var getTimeTillRio = (userDate - now) / 1000;
+
+// var centiseconds = Math.floor((getTimeTillRio * 100) % 100);
 var seconds = Math.floor(getTimeTillRio % 60);
 var minutes = Math.floor((getTimeTillRio / 60) % 60);
 var hours = Math.floor(((getTimeTillRio / 60) / 60) % 24);
 var days = Math.floor((((getTimeTillRio / 60) / 60) / 24) % 7);
 var weeks = Math.floor(((((getTimeTillRio / 60) / 60) / 24) / 7));
+
+
+
 
 var lineUp = ['THE KILLERS', 'TWENTY 0NE PIL0TS', 'BECK', 'DEADMAU5', 'THE LUMINEERS', 'G-EASY', 'ALABAMA SHAKES', 'COLLEGROVE (LIL WAYNE AND 2CHAINZ)', 'BAND OF HORSES', 'JAMES BAY', 'KESHA', 'BIG BOI', 'CHVRCHES', 'LOGIC', 'GROUPLOVE', 'CITY AND COLOUR', 'NATHANIEL RATELIFF & THE NIGHT SWEATS', 'LEON BRIDGES', 'DJ MUSTARD', 'DNCE', 'MELANIE MARTINEZ', 'RAURY', 'DAYA', 'ST. LUCIA', 'MAYER HAWTHORNE', 'LUCIUS', 'CORINNE BAILEY RAE', 'PETE YORN', 'ZELLA DAY', 'PETER BJORN AND JOHN', 'JOHNNYSWIM', 'JOSEPH', 'NF', 'THE COATHANGERS', 'BALKAN BEAT BOX', 'THE SHADOWBOXERS']
 
@@ -23,12 +40,24 @@ if (i === lineUp.length){
 	i = 0;
 }
 console.log(i);
+// document.getElementsByClassName('centiseconds-number')[0].innerHTML = centiseconds;
 document.getElementsByClassName('seconds-number')[0].innerHTML = seconds;
 document.getElementsByClassName('minutes-number')[0].innerHTML = minutes;
 document.getElementsByClassName('hours-number')[0].innerHTML = hours;
 document.getElementsByClassName('days-number')[0].innerHTML = days;
 document.getElementsByClassName('weeks-number')[0].innerHTML = weeks;
+
+if (isNaN(userDate)){
+document.getElementsByClassName('seconds-number')[0].innerHTML = 00;
+document.getElementsByClassName('minutes-number')[0].innerHTML = 00;
+document.getElementsByClassName('hours-number')[0].innerHTML = 00;
+document.getElementsByClassName('days-number')[0].innerHTML = 00;
+document.getElementsByClassName('weeks-number')[0].innerHTML = 00;
 }
+
+} //closing bracket for update();
+
+
 
 window.setInterval(update, 1000);
 
